16a293c79f1195860fdeded252a6214fbf4385ae
SS-victoria <stiven-cool@hotmail.com>
2023-06-16 12:45:07 -0500

    actualizacion

--- js/transition.js
+++ js/transition.js
@@ -1,6 +1,6 @@
 let boxToggle = document.getElementById("boxToggle");
 let toggle = document.getElementById("toggle");
-
+let obs = document.getElementById("obs")
 
 boxToggle.addEventListener("click", function(){
     if (toggle.classList.contains("toggle-off")){
@@ -8,12 +8,17 @@ boxToggle.addEventListener("click", function(){
         toggle.classList.remove("toggle-off");
         boxToggle.classList.add("box-toggle-on");
         boxToggle.classList.remove("box-toggle-off");
+        obs.classList.add("obs-on");
+        obs.classList.remove("obs");
+
     }
     else{
         toggle.classList.remove("toggle-on")
         toggle.classList.add("toggle-off")
         boxToggle.classList.remove("box-toggle-on");
         boxToggle.classList.add("box-toggle-off");
+        obs.classList.add("obs");
+        obs.classList.remove("obs-on");
 
     }
 })
